8c2dd59761c9536bee2868576b9b2230588f38c7
pablo7008 <76118687+pablo7008@users.noreply.github.com>
2021-07-29 17:56:04 -0300

    Clases Usuario, Sesion

---
+++ FormsPPAI/Entity/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashbord.Entity
+{
+    public class Usuario
+    {
+        private int caducidad;
+        private int contraseña;
+        private int nombre;
+
+        public int Caducidad
+        {
+            get => caducidad;
+            set => caducidad = value;
+        }
+        public int Contraseña
+        {
+            get => contraseña;
+            set => contraseña = value;
+        }
+        public int Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+    }
+}
