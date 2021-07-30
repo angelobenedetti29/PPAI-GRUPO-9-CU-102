ad5ad8d11873ec87fadd032238786e90be58f56c
angelo <angelo.benedetti2910@gmail.com>
2021-07-30 16:04:43 -0300

    primera class

---
+++ FormsPPAI/Entity/Obra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashbord.Entity
+{
+    class Obra
+    {
+        public float Alto { get; set; }
+        public float Ancho { get; set; }
+        public int CodSensor { get; set; }
+        public int DuracionExtendida { get; set; }
+        public int DuracionReducida { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaPrimerIngreso { get; set; }
+        public string NombreObra { get; set; }
+        public float Peso { get; set; }
+        public float valuacion { get; set; }
+    }
+}
